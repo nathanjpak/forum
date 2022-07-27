13abd2edb0225a82221f162148139fab29586163
nathanjpak <nathan.j.pak@gmail.com>
2022-07-27 11:38:11 -0700

    add function to submit and display posts

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 // your code here
+const posts = document.getElementsByClassName("posts")[0];
+const name = document.getElementById("name");
+const message = document.getElementById("message");
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", function() {
+    const post = document.createElement("div");
+    post.append(name.value, message.value);
+    posts.appendChild(post);
+});
