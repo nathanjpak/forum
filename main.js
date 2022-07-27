2f43ad27919ece57e9eb35145f63ef68523a13d4
nathanjpak <nathan.j.pak@gmail.com>
2022-07-27 16:05:05 -0700

    add upvote buttons

--- main.js
+++ main.js
@@ -6,6 +6,41 @@ const submit = document.getElementById("submit");
 
 submit.addEventListener("click", function() {
     const post = document.createElement("div");
-    post.append(name.value, message.value);
+    post.classList.add("row");  
+    
+    const content = document.createElement("div");
+    content.classList.add("col-sm-11");
+
+    const author = document.createElement("p");
+    author.innerHTML = `Posted by: <strong>${name.value}</strong>`;
+    content.append(message.value, author);    
+
+    //add upvote and downvote buttons
+    const buttons = document.createElement("div");
+    buttons.classList.add("col-sm-1", "text-center"); 
+    const upvote = document.createElement("button");
+    upvote.innerHTML = '<i class="fa fa-angle-up"></i>';
+    upvote.classList.add("btn", "btn-info");
+    const downvote = document.createElement("button");
+    downvote.innerHTML = '<i class="fa fa-angle-down"></i>';
+    downvote.classList.add("btn", "btn-info");
+    
+    let upvotes = 0;
+    const score = document.createElement("p");
+    score.textContent = upvotes;
+    score.style.marginTop = "10px";
+    buttons.append(upvote, score, downvote); 
+
+    //button functionality
+    upvote.addEventListener("click", function() {
+        upvotes++;
+        score.textContent = upvotes;
+    });
+    downvote.addEventListener("click", function() {
+        upvotes--;
+        score.textContent = upvotes;
+    });
+
+    post.append(buttons, content);
     posts.appendChild(post);
 });
